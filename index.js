6b1dc3ae9d2f488189b795fb50442e48189cd3eb
furqannr <furqannr@gmail.com>
2023-02-02 20:11:55 +0500

    For npx

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 // This somewhat complex TypeScript/Node.js project is a console-based application. When the system starts the user is prompted with
 // a user id and user pin. After entering the details successfully, the ATM functionalities are unlocked. All the user data is
 // generated randomly.
